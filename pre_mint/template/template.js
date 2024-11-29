72641cf2f1b0c8155bf2ce04a6a703d4ad9456cc
pranjalkumarjha <pranjalkumarjsspk@gmail.com>
2024-11-29 15:07:04 +0530

    demo purpose

---
+++ pre_mint/template/template.js
@@ -0,0 +1,10 @@
+// Need for this js is 
+// Datastructure to maintain the table contents
+// manipulate the spa for the changes based on button clicks 
+let dutch_auction_rule_button = document.getElementById('dutch-auction-rule'); 
+let connect_button = document.getElementById('connect-button'); 
+let conquerer_secured_txt = document.getElementById('conquerer-secured'); 
+let current_recent_price_toggle = document.getElementById('current-recent-price-toggle'); 
+let price = document.getElementById('price'); 
+let current_value = document.getElementById('current-value'); 
+let total_value = document.getElementById('total-value'); 
